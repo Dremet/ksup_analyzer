7aef2e8b393b0e2d079bd8c9e28fd545baf977ee
m4rc0-r <devnull.mre@gmail.com>
2023-07-20 17:01:06 +0200

    revert changes in run.py file

--- run.py
+++ run.py
@@ -5,13 +5,12 @@ from csup_analyzer.plots.Plots import GapToWinnerTablePlot, LapPositionTablePlot
 
 fh = FileHandler(
     [
-        #"example_replay_files/from_patch_1_5_0/20230716T21-06-01Z.header",
-        "replay_files/20230719T21-52-53Z.header",
+        "example_replay_files/from_patch_1_5_0/20230716T21-06-01Z.header",
+        "example_replay_files/from_patch_1_5_0/20230621T19-22-24Z.header",
     ]
 )
 
-#quali = Quali(fh.get_quali_file_content())
-quali = None
+quali = Quali(fh.get_quali_file_content())
 race = Race(fh.get_race_file_content())
 lineup = LineUp(fh.get_race_file_content())
 
@@ -19,19 +18,19 @@ event = Event(lineup=lineup, race=race, quali=quali)
 event.create_result_dataframe()
 event.run_result_calculations()
 
-#print(event.result_df)
-#print(event.result_df.lap_position_table)
+print(event.result_df)
+print(event.result_df.lap_position_table)
 
-#print(
-#    event.result_df[
-#        ["name", "fastest_lap_time_quali", "starting_position_race"]
-#    ].sort_values(by="starting_position_race")
-#)
+print(
+    event.result_df[
+        ["name", "fastest_lap_time_quali", "starting_position_race"]
+    ].sort_values(by="starting_position_race")
+)
 
 gapToWinnerPlot = GapToWinnerTablePlot(event.result_df, race)
 gapToWinnerPlot.plot(ymin=-5, ymax=15)
 lapPositionPlot = LapPositionTablePlot(event.result_df, race)
-lapPositionPlot.plot()
+lapPositionPlot.plot("Example_Replay")
 gapToLeaderPlot = GapToLeaderTablePlot(event.result_df, race)
 gapToLeaderPlot.plot(ymax=15)
 pass
